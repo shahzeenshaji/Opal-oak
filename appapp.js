1c0684bcc296c2d13378e172a7ae09179f5fbfe2
shahzeenshaji <shahzeen.shaji@bcah.christuniversity.in>
2025-03-07 14:01:08 +0530

    Add files via upload

--- appapp.js
+++ appapp.js
@@ -4,23 +4,15 @@ app.controller('StoreController', function ($scope, $http) {
     $scope.products = [];
     $scope.customers = [];
 
-   
-    $http.get('clothing.json').then(function (response) {
-        $scope.products = response.data.products;
-        $scope.customers = response.data.customers;
-
-        $scope.$broadcast('jsonLoaded', response.data);
-    });
-
-    
-    $scope.$on('jsonLoaded', function (event, data) {
-        console.log("JSON Data Loaded:", data);
-    });
-
-    
-    $scope.$watch('products', function (newVal, oldVal) {
-        if (newVal !== oldVal) {
-            console.log("Product data updated!");
+    $http.get('clothing.json').then(
+        function (response) {
+            console.log("JSON Data Loaded:", response.data); // Debugging Log
+            $scope.products = response.data.products;
+            $scope.customers = response.data.customers;
+        },
+        function (error) {
+            console.error("Error loading JSON:", error);
         }
-    }, true);
+    );
 });
+
